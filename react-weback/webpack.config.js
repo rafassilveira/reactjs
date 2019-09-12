caa8f6aa57783d9875ed8aaa20c6dd98fcba5013
Rafael silveira <rafaelssilveira@outlook.com>
2019-09-12 16:08:13 +0000

    webpack

--- react-weback/webpack.config.js
+++ react-weback/webpack.config.js
@@ -6,6 +6,8 @@ module.exports = {
     entry:path.join(__dirname,'src','index'),
     output:{
         path:path.join(__dirname,'dist'),
-        filename:'bundle.js'
+        filename:'bundle.js',
+		publicPath:'/static'
     }
+	
 }
