346938a66970bf080e7c91b07a9149a89bed3b4e
Rafael Silveira <rafaelssilveira@outlook.com>
2020-07-01 23:02:32 -0300

    componentes es2015

--- Modulo-1/learning-props/src/App.js
+++ Modulo-1/learning-props/src/App.js
@@ -1,17 +1,27 @@
-import React from "react";
+import React, { Component } from "react";
 import Title from "./Title";
 
-function App() {
-  return (
-    <div>
-      <Title name={"Rafael"} lastname={"Silveira"} />
-      <label htmlFor="input" data-label="Label">
-        {" "}
-        Input
-      </label>
-      <input type="text" id="input" aria-hidden={true} />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <Title name={"Fernando"} />
+      </div>
+    );
+  }
 }
+
+// function App() {
+//   return (
+//     <div>
+//       <Title name={"Rafael"} lastname={"Silveira"} />
+//       <label htmlFor="input" data-label="Label">
+//         {" "}
+//         Input
+//       </label>
+//       <input type="text" id="input" aria-hidden={true} />
+//     </div>
+//   );
+// }
 
 export default App;
